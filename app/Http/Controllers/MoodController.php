92cc74d5d09f31f1158b2237f8598ad288cc8686
RettaKresensia <rettakresensia@gmail.com>
2026-02-24 20:27:56 +0700

    Update MoodController

--- app/Http/Controllers/MoodController.php
+++ app/Http/Controllers/MoodController.php
@@ -43,7 +43,7 @@ class MoodController extends Controller
             "Terima kasih sudah bertahan sejauh ini. Kamu hebat! 🏅"
         ];
 
-        $randomQuote = $quotes[array_rand($quotes)];
+        $randomQ uote = $quotes[array_rand($quotes)];
         $moods = Mood::where('user_id', Auth::id())->latest()->get();
 
         return view('dashboard', compact('randomQuote', 'moods'));
